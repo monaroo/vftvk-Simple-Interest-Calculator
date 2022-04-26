c5d9979766897974803f80b1137dca2fffe0e685
Mona White <monarooster@gmail.com>
2022-04-25 21:42:08 -0600

    different js file

--- script.js
+++ script.js
@@ -3,33 +3,19 @@ function compute()
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
-    
-    if (principal.value == "") {
-        alert("Please enter the principal.");
-        fp.focus();
-        return false;
-    }
-    if (rate.value == "") {
-        alert("Please select the rate.");
-        fr.focus();
-        return false;
-    }
-    if (years.value == "") {
-        alert("Please enter the number of years.");
-        ft.focus();
-        return false;
-    }
-
     var interest = principal * years * rate / 100;
     var year = new Date().getFullYear()+parseInt(years);
 }
-
 function updateRate()
 {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;    
+    document.getElementById("rate_val").innerText=rateval;
+    
 }
-
 var rangeinput = document.getElementById("rate");
 rangeinput.onchange = updateRate;
-        
+
+var result = document.getElementById("result");
+result.onclick = compute;
+
+document.getElementById("result").innerHTML="If you deposit"+principal+",\<br\> at an interest rate of"+rate+", \<br\> you will receive an amount of"+result+"\<br\> in the year"+year;
