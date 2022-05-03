da5cf3e7f8f4b16dc45ba2dbc6a816d708f9ed67
Mona White <monarooster@gmail.com>
2022-05-03 17:35:25 -0600

    all done yay

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
 
 
 //let year = new Date().getFullYear()+parseInt();
-let btn = document.createElement("button");
+let btn = document.getElementById("compute");
 //var result = document.getElementById("result");
-btn.onclick = function compute()
+btn.onclick = function ()
 {
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     let years = document.getElementById("years").value;
     var interest = principal * years * rate / 100;
-    let year = new Date().getFullYear()+parseInt();
+    let year = new Date().getFullYear();
+
+    //debugValue("years", years);
+    //debugValue("year", year);
+    //debugValue("Number(years)", Number(years));
+    //debugValue("Number(years)+year", Number(years)+year);
+
+
+    document.getElementById("result").innerHTML = "If you deposit $"+principal+
+        "\<br\> at an interest rate of "+rate+
+        "% , \<br\> you will receive an amount of $"+interest+
+        "\<br\> in the year "+(Number(years)+year);
 }
+
+function debugValue(name, value) {
+    console.log(name, value, typeof(value));
+}
+
 function updateRate()
 {
     var rateval = document.getElementById("rate").value;
@@ -22,4 +38,4 @@ rangeinput.onchange = updateRate;
 
 
 
-document.getElementById("result").innerHTML="If you deposit"+principal+",\<br\> at an interest rate of"+rate+", \<br\> you will receive an amount of"+result+"\<br\> in the year"+Number(years)+year;
+//document.getElementById("result").innerHTML="If you deposit"+principal+",\<br\> at an interest rate of"+rate+", \<br\> you will receive an amount of"+result+"\<br\> in the year"+Number(years)+year;
